bcff465cd477996c14b163381ae07f39247d3768
Mbarek, Mohamed Ali (060-Extern) <mbarek@kabelmail.de>
2017-12-05 21:54:59 +0100

    @PersistenceContext(unitName = "EmployeeDS", type = PersistenceContextType.EXTENDED)

--- employee-app/dao/src/main/java/com/it2go/employee/dao/EntityManagerProducecr.java
+++ employee-app/dao/src/main/java/com/it2go/employee/dao/EntityManagerProducecr.java
@@ -3,10 +3,11 @@ package com.it2go.employee.dao;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
 public class EntityManagerProducecr {
 
-    @PersistenceContext(unitName = "EmployeeDS")
+    @PersistenceContext(unitName = "EmployeeDS", type = PersistenceContextType.EXTENDED)
     private EntityManager defaultEntityManager;
 
     @Produces
