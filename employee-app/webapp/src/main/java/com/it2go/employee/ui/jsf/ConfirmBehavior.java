38365c863267d5c8a61af4ab1b3465628e312114
Mbarek, Mohamed Ali (060-Extern) <mbarek@kabelmail.de>
2018-01-01 22:23:31 +0100

    merge with branch1 async confirm dialog added. to be continued...

--- employee-app/webapp/src/main/java/com/it2go/employee/ui/jsf/ConfirmBehavior.java
+++ employee-app/webapp/src/main/java/com/it2go/employee/ui/jsf/ConfirmBehavior.java
@@ -1,17 +1,44 @@
 package com.it2go.employee.ui.jsf;
 
+import javax.faces.application.ResourceDependency;
 import javax.faces.component.behavior.ClientBehaviorBase;
 import javax.faces.component.behavior.ClientBehaviorContext;
 import javax.faces.component.behavior.FacesBehavior;
 
 @FacesBehavior("employee.behavior.Confirm")
+//@ResourceDependency(name = "confirm.js",target = "head")
+@ResourceDependency(name="jsf.js", library="javax.faces", target="head")
 public class ConfirmBehavior extends ClientBehaviorBase {
+
+    private String message;
+    private boolean ajax=false;
 
     @Override
     public String getScript(ClientBehaviorContext behaviorContext) {
 
         //return "return confirm('Are you sure ?');";
         //return "$('#myModal').modal(null);";
-        return " ShowConfirmYesNo();";
+        String yesFunc = "yesFunction";
+        if(this.ajax)
+            yesFunc = "yesFunctionAjax";
+
+        return " ShowConfirmYesNo(event, "+yesFunc+", null, '"+this.message+"');";
+        //return " ShowConfirmYesNo(event);";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isAjax() {
+        return ajax;
+    }
+
+    public void setAjax(boolean ajax) {
+        this.ajax = ajax;
     }
 }
