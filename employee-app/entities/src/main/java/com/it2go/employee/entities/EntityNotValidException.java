1c550a0010fb637296dbd79381105eb61d8463a9
Ali Mbarek <Mohammed-Ali.Mbarek@baden-badener.de>
2018-01-26 17:36:16 +0100

    new base class for exceptions with id

--- employee-app/entities/src/main/java/com/it2go/employee/entities/EntityNotValidException.java
+++ employee-app/entities/src/main/java/com/it2go/employee/entities/EntityNotValidException.java
@@ -1,6 +1,21 @@
 package com.it2go.employee.entities;
 
-public class EntityNotValidException extends Exception {
+import com.it2go.framework.dao.BaseException;
+
+public class EntityNotValidException extends BaseException {
+
+    public EntityNotValidException() {
+        super();
+    }
+
+    public EntityNotValidException(String message) {
+        super(message);
+    }
+
+    public EntityNotValidException(String errorCode, String message) {
+        super(errorCode, message);
+    }
+
     @Override
     public String getMessage() {
         return "Entity not valid!";
