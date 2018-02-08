bf335395bf4bd03bc426338e5608eb31337c181e
Ali Mbarek <Mohammed-Ali.Mbarek@baden-badener.de>
2018-02-08 17:47:48 +0100

    projects work on. Exception handling and error page to continue

--- employee-app/entities/src/main/java/com/it2go/employee/entities/Project.java
+++ employee-app/entities/src/main/java/com/it2go/employee/entities/Project.java
@@ -3,10 +3,16 @@ package com.it2go.employee.entities;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
@@ -15,10 +21,33 @@ import java.util.Date;
 @Entity
 public class Project extends DomainEntity {
 
+    @Basic
+    @Column(name = "NAME", nullable = false)
+    @Size(min = 3, max = 100)
+    @NotNull
     private String name;
+
+    @Basic
+    @Column(name = "BUDGET")
     private double budget;
+
+    @Basic
+    @Column(name = "BEGIN")
+    @Temporal(TemporalType.DATE)
     private Date begin;
+
+    @Basic
+    @Column(name = "END")
+    @Temporal(TemporalType.DATE)
     private Date end;
+
+    @Column(name = "TIL_TODAY")
+    private boolean tilToday;
+
+    @Basic
+    @Column(name = "DESCRIPTION")
+    @Size(min = 50, max = 500)
+    private String description;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "EMPL_ID")
