7835efd1888c9339e0302e879551bc1a71408eaf
Ali Mbarek <Mohammed-Ali.Mbarek@baden-badener.de>
2018-02-08 17:47:48 +0100

    prime tests

--- employee-app/persistence/src/main/java/com/it2go/employee/persistence/IProjectRepository.java
+++ employee-app/persistence/src/main/java/com/it2go/employee/persistence/IProjectRepository.java
@@ -1,9 +1,7 @@
 package com.it2go.employee.persistence;
 
 
-import com.it2go.employee.entities.Person;
 import com.it2go.employee.entities.Project;
-import com.it2go.framework.persistence.IEntityRepository;
 
 import java.time.LocalDate;
 import java.util.List;
