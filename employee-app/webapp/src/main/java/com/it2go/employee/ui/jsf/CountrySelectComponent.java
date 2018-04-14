6287172bf6688779138b9c1a7dd54a75ba37c5d3
Mbarek, Mohamed Ali (060-Extern) <mbarek@kabelmail.de>
2018-04-15 00:04:52 +0200

    work on countrySelect

--- employee-app/webapp/src/main/java/com/it2go/employee/ui/jsf/CountrySelectComponent.java
+++ employee-app/webapp/src/main/java/com/it2go/employee/ui/jsf/CountrySelectComponent.java
@@ -1,12 +1,18 @@
 package com.it2go.employee.ui.jsf;
 
 import com.it2go.masterdata.Continent;
+import com.it2go.masterdata.Country;
 import lombok.Data;
 
 import javax.faces.component.FacesComponent;
 import javax.faces.component.NamingContainer;
 import javax.faces.component.UIInput;
 import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Data
 @FacesComponent(value = "countrySelect")
@@ -25,5 +31,26 @@ public class CountrySelectComponent extends UIInputComponent implements NamingCo
         return UINamingContainer.COMPONENT_FAMILY;
     }
 
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException {
+        String countryCode = (String)getValue();
+        if(countryCode != null && !countryCode.isEmpty()){
+            Continent continent = Continent.getContinentContainingCountry(countryCode);
 
+            if(continent != null)
+                continentSelect.setValue(continent.getCode());
+        }
+        else
+            continentSelect.setValue(null);
+
+        super.encodeBegin(context);
+    }
+
+    public List<Country> getCountries(){
+        if(continentSelect.getValue() != null)
+            return Objects.requireNonNull(Continent.getContinentWithCode((String) continentSelect.getValue()))
+                    .getCountries(FacesContext.getCurrentInstance().getViewRoot().getLocale());
+
+        return new ArrayList<>();
+    }
 }
